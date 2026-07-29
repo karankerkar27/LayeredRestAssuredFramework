8b25e338987120740b9b1da082c86248a9a662fa
karankerkar27 <karankerkar1109@gmail.com>
2026-07-29 23:12:04 +0530

    Auth api commit along with folder structure, project setup for maven

---
+++ src/main/java/resSpec/RequestSpec.java
@@ -0,0 +1,20 @@
+package resSpec;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+
+public class RequestSpec {
+
+    public RequestSpecification resSpecAuth(String baseUrl, Object obj){
+
+        RequestSpecification request = RestAssured.given();
+        request.spec(new RequestSpecBuilder().setContentType(ContentType.JSON).build().log().all());
+        request.baseUri(baseUrl);
+        request.body(obj);
+
+        return request;
+
+    }
+}
---
+++ src/main/resources/feature/authTest.feature
@@ -0,0 +1,8 @@
+@AuthFeature
+  Feature: Authorization Feature
+
+
+
+    @Auth
+    Scenario: Creating Auth token
+      Given All details for authorization api is correct
